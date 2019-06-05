990959221f3132f771a7939b82f9faf9d517633d
MagicWoon <46014722+MagicWoon@users.noreply.github.com>
2019-06-05 12:46:23 +0800

    Update SpringbootHello01ApplicationTests.java

--- src/test/java/com/wh/springboothello01/SpringbootHello01ApplicationTests.java
+++ src/test/java/com/wh/springboothello01/SpringbootHello01ApplicationTests.java
@@ -11,7 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
-
+/**
+* 测试类
+*/
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootHello01ApplicationTests {
